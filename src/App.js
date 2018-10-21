e0210fd0ffbb6f7664c6679e04d291725f25de88
Shion Kou <cyg65@foxmail.com>
2018-10-22 02:45:07 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,200 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <div className="container-fluid main d-flex row m-auto text-black-50">
+        <div className="container col-sm-12 col-lg-4 m-0 mt-5">
+          <div className="display-4">
+            <b>SHION KOU</b>
+          </div>
+          <div>Experience Designer</div>
+          <p className="mt-3">
+            I am a product/UX/web designer based in Tokyo. I am passionate for
+            all things technology, creative.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        </div>
+
+        <div className="container col-sm-12 col-lg-8 m-0 d-flex row mt-5 right">
+          <div className="content card-columns">
+            <a
+              href="https://cyg65.github.io/kisetsu_2018/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="card mb-5"
+            >
+              <img
+                class="card-img-top"
+                src={require('./assets/Kisetsu.png')}
+                alt="Kumano Fude: Shiki"
+              />
+              <div class="card-body">
+                <h5 class="card-title">[WIP]Kumano Fude: Shiki</h5>
+                <p class="card-text">
+                  <ul>
+                    <li>Prototype: 3D (Cinema4d)</li>
+                    <li>Video: Cinema4D + After Effects</li>
+                    <li>Landing Page: HTMLCSS+Jquery</li>
+                    <li>Design: Cinema4d + Illustrator</li>
+                  </ul>
+                  In collebortaion with © 2003-2018 Neowing. All Rights
+                  Reserved.
+                </p>
+              </div>
+            </a>
+            <a
+              href="http://www.cdjapan.co.jp/beauty/kisetsu2017_konayuki_en.html"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="card mb-5"
+            >
+              <img
+                class="card-img-top"
+                src={require('./assets/Konayuki.png')}
+                alt="Kumano Fude: Konayuki"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Kumano Fude: Konayuki</h5>
+                <p class="card-text">
+                  <ul>
+                    <li>Video: After Effects + Premiere Pro</li>
+                    <li>Landing Page: HTMLCSS + W3-Templete + JS</li>
+                    <li>Design: Photoshop + Illustrator</li>
+                  </ul>
+                  In collebortaion with (Designer) Shawn Han, © 2003-2018
+                  Neowing. All Rights Reserved.
+                </p>
+              </div>
+            </a>
+            <a className="card mb-5">
+              <img
+                class="card-img-top"
+                src={require('./assets/lollilip_thumb.jpg')}
+                alt="T-shirts: Lollilip"
+              />
+              <div class="card-body">
+                <h5 class="card-title">[WIP]T-shirts: Lollilip</h5>
+                <p class="card-text">
+                  <ul>
+                    <li>Design: Photoshop + Illustrator</li>
+                  </ul>
+                  In collebortaion with (Illustrator) Shen Lee, © 2003-2018
+                  Lollilip China Ltd. All Rights Reserved.
+                </p>
+              </div>
+            </a>
+
+            <a
+              href="http://www.cdjapan.co.jp/beauty/yoshiki.html"
+              className="card mb-5"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                class="card-img-top"
+                src={require('./assets/Yoshiki.png')}
+                alt="Kumano Fude: Yoshiki"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Kumano Fude: Yoshiki</h5>
+                <p class="card-text">
+                  <ul>
+                    <li>Photography Direction</li>
+                    <li>Landing Page: HTMLCSS + W3-Templete + JS</li>
+                    <li>Design: Photoshop + Illustrator</li>
+                  </ul>
+                  In collebortaion with © 2010 Koyudo All Rights Reserved.
+                </p>
+              </div>
+            </a>
+            <a
+              href="https://cyg65.github.io/ChaDJ_LP_MKI/"
+              className="card mb-5"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                class="card-img-top"
+                src={require('./assets/ChaDJ.png')}
+                alt="ChaDJ"
+              />
+              <div class="card-body">
+                <h5 class="card-title">[WIP]Branding: ChaDJ</h5>
+                <p class="card-text">
+                  <ul>
+                    <li>Landing Page: HTMLCSS + Bootstrap + JS</li>
+                    <li>Logo Design: Illustrator</li>
+                  </ul>
+                  In collebortaion with: © 2010 Neowing. All Rights Reserved.
+                </p>
+              </div>
+            </a>
+            <div className="card mb-5">
+              <img
+                class="card-img-top"
+                src={require('./assets/Cdj_Production_logo.png')}
+                alt="cdj productions"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Branding: CDJ Productions </h5>
+                <p class="card-text">
+                  <ul>
+                    <li>Logo Design: Illustrator</li>
+                    <li>Video: After Effects</li>
+                  </ul>
+                  In collebortaion with: © 2010 Neowing. All Rights Reserved.
+                </p>
+              </div>
+            </div>
+            <div className="card mb-5">
+              <iframe
+                title="demogif"
+                src="https://giphy.com/embed/9P1p6zllxm4oc9fKG6"
+                width="220"
+                height="480"
+                frameBorder="0"
+                class="card-img-top"
+                allowFullScreen
+              />
+              <div class="card-body">
+                <h5 class="card-title">JS Sidebar Widget</h5>
+                <p class="card-text">
+                  <ul>
+                    <li>Perl+JS</li>
+                  </ul>
+                  In collebortaion with: © 2010 Neowing. All Rights Reserved.
+                </p>
+              </div>
+            </div>
+            <div className="card mb-5">
+              <img
+                src={require('./assets/ProxyJapan.gif')}
+                width="220"
+                class="card-img-top"
+                alt="react native demo"
+              />
+              <div class="card-body">
+                <h5 class="card-title">[WIP]Proxy Japan Mobile APP</h5>
+                <p class="card-text">
+                  <ul>
+                    <li>React Native</li>
+                  </ul>
+                  In collebortaion with: © 2010 Neowing. All Rights Reserved.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <BrowserRouter>
+          <Navigation />
+          <Switch>
+            <Route />
+          </Switch>
+        </BrowserRouter> */}
       </div>
     );
   }
